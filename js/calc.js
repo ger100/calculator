f0a0fb497958848172bf2093634d7a4fa49a6d83
V.Gergely <kojak105@gmail.com>
2020-11-30 11:01:19 +0100

    C button

--- js/calc.js
+++ js/calc.js
@@ -1,12 +1,25 @@
 const buttons = document.querySelectorAll('.btn, .btn-op')
 const display = document.querySelector('.display')
-
-let displayData = "";
+let calcOnDisplay = "";
 
 buttons.forEach(button => {
   button.addEventListener('click', () => { 
     const buttonValue = button.getAttribute('data-value');  
-    displayData += buttonValue;
-    display.textContent = displayData;
+    calcOnDisplay += buttonValue;
+    display.textContent = calcOnDisplay;
   })
+})
+
+
+const btnClear = document.querySelector('.clear')
+btnClear.addEventListener('click', () => { 
+    calcOnDisplay = "";
+    display.textContent = calcOnDisplay;
+  })
+
+const btnEqual = document.querySelector('.button2--equal')
+btnEqual.addEventListener('click', () => { 
+    // use the eval() function to evaluate the expression and output it to the display
+    calcOnDisplay=eval(calcOnDisplay)
+    display.textContent = calcOnDisplay
 })
